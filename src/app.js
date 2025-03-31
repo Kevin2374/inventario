6787bf2e1b9305f1f64a0d0654feb6e2a784f8ee
Kevin2374 <kevinjhampier5@gmail.com>
2025-03-30 23:56:11 -0500

    enrutador y controladores

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 /* Importamos al framework express */
 import express from "express";
-
+import categoriasRoutes from "./routes/categorias.routes.js"
 const app = express();
 
 app.set("port",5000)
 
+app.use("/api/categorias",categoriasRoutes)
 /* hacemos disponible a mi server app para toda la aplicacion */
 export default app;
---
+++ src/controllers/categoria.controllers.js
@@ -0,0 +1,7 @@
+const getCategorias = (req, rest)=>{
+    rest.json({"Categoria":"Electrodomesticos"})
+
+}
+export const methodHTPP = {
+    getCategorias
+}
